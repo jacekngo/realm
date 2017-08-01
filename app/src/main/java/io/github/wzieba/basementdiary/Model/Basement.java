4c59c9121a10f167f45b79b50da999b329236b87
jacek <jacekngo@gmail.com>
2017-08-01 19:38:44 +0200

    <><

---
+++ app/src/main/java/io/github/wzieba/basementdiary/Model/Basement.java
@@ -0,0 +1,8 @@
+package io.github.wzieba.basementdiary.Model;
+
+/**
+ * Created by RENT on 2017-08-01.
+ */
+
+public class Basement {
+}
---
+++ app/src/main/java/io/github/wzieba/basementdiary/MyApplication.java
@@ -0,0 +1,8 @@
+package io.github.wzieba.basementdiary;
+
+/**
+ * Created by RENT on 2017-08-01.
+ */
+
+public class MyApplication {
+}
